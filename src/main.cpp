889f8cab0448aab0e34a688d8f8144930bdbe5e9
ZaraSamoo <141186185+ZaraSamoo@users.noreply.github.com>
2025-12-18 02:45:57 +0500

    Add BFS and DFS algorithms for map traversal
    Implement BFS and DFS for GridMap navigation

--- src/main.cpp
+++ src/main.cpp
@@ -1,1 +1,33 @@
+#include <iostream>
+#include "GridMap.h"
+
+using namespace std;
+
+int main() {
+    GridMap map;
+
+    // Load the GIKI map
+    if (!map.loadFromFile("data/maps/giki_map.txt")) {
+        cout << "Map loading failed\n";
+        return 1;
+    }
+
+    int startRow = 0, startCol = 0;                    // Start at top-left
+    int goalRow = map.getRows() - 1;                  // Goal at bottom-right
+    int goalCol = map.getCols() - 1;
+
+    // BFS: Check if goal is reachable
+    if (bfs(map, startRow, startCol, goalRow, goalCol)) {
+        cout << "BFS: Goal is reachable.\n";
+    } else {
+        cout << "BFS: Goal is not reachable.\n";
+    }
+
+    // DFS: Mark all reachable cells from start
+    bool visited[1000][1000] = {false};
+    dfs(map, startRow, startCol, visited);
+    cout << "DFS traversal completed.\n";
+
+    return 0;
+}
 
